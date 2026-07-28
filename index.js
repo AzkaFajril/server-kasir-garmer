d906cdd62dfb03c3e0552c2f544448ee8eec41dd
azka <azkafajril015@gmail.com>
2026-07-28 13:07:36 +0700

    123213

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use((req, res, next) => {
   next();
 });
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'http://localhost:5173, https://kasirtokogarmer.netlify.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
